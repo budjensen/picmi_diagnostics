Fix bug in J_d calculation resulting in zero J_d
* Previously, the E_last_step array was being overwritten
leading to the displacement current to always be zero. This
commit copies the contents of the array, not the array location.

main.py
@@ -2175,7 +2175,7 @@ class Diagnostics1D:
 
         # Save the electric field for the displacement current
         if save_E_last_step:
-            self.E_z_last_step = self._current_Ez_data
+            np.copyto(self.E_z_last_step, self._current_Ez_data)
 
         # Finalize and save diagnostics
         if step == self.diag_stop[self.curr_diag_output]: